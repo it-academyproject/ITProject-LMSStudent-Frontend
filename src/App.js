887e85594797252b4d722e87d8c0bc4261be6f10
Adria Pujol <adriapujolaparicio@gmail.com>
2020-09-12 15:47:44 +0200

    navbar and footer, react bootstrap, react icons, bootstrap, dependecies, changes to App and index.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-
+      <Header></Header>
+      <main className="content"></main>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.scss';
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <div className="footer-container d-flex justify-content-center align-items-center">
+                <p className="footer-text m-0">Some random text should be here</p>
+            </div>    
+        </footer>
+    )
+}
+
+export default Footer;
