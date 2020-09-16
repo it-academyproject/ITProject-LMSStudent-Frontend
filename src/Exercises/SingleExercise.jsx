4681603ee21e7c0816362d892af0980dbdc8fad7
Adria Pujol <adriapujolaparicio@gmail.com>
2020-09-16 14:45:28 +0200

    form with basic fields, no styling at all

--- src/Exercises/SingleExercise.jsx
+++ src/Exercises/SingleExercise.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 const SingleExercise = () => {
+
+    const topicList = ["Javascript", ".NET", "Bootstrap", "CSS", "HTML"];
+
+
     return (
         <Form>
-            
+            <Form.Group className="Group">
+                <Form.Label>Title:</Form.Label>
+                <Form.Control placeholder="Exercise title..." />
+            </Form.Group>
+            <Form.Group className="Group">
+                <Form.Label>Topic:</Form.Label>
+                <Form.Control as="select" defaultValue="Choose...">
+                    <option>Select a topic...</option>
+                    {topicList.map((topic, index) => <option key={index}>{topic}</option>)}
+                </Form.Control>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Estimated days:</Form.Label>
+                <Form.Control type="number" min={1} />
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>File:</Form.Label>
+                <Form.File />
+            </Form.Group>
+            <Button className="btn-style">Upload</Button>
+            <Button className="btn-style">Delete</Button>
         </Form>
     )
 }
